ae28c49a72b22b5a7e62ca28ed4fa71e44055a3a
michael chrisco <michaelachrisco@gmail.com>
2016-10-28 14:11:08 -0700

    Pull master (#1)
    * xCow example
    
    * passing cow example
    
    * Initial commit of script/ubuntu/15.10/setup-prereqs.bash
    
    * More on setup-prereqs.bash
    
    * Line endings, Ubuntu 16.04 script, Node v4.4 for Travis (#3)
    
    * Run npm with sudo
    
    * Don't run npm with sudo
    
    * Add .gitattributes with EOL settings for various file types
    
    * Rename setup-prereqs.bash -> setup-prereqs.sh; make a few other changes
    
    * Automatically update npm to version 3
    
    * Split up apt-get installation of npm and mocha, since installing them together doesn't seem to be working
    
    * Add Ubuntu 16.04 setup script
    
    * More on Ubuntu 16.04 script
    
    * Add Node v4.4 to Travis
    
    * Make script/ubuntu/16.04/setup-prereqs.sh executable (#4)
    
    * Run npm with sudo
    
    * Don't run npm with sudo
    
    * Add .gitattributes with EOL settings for various file types
    
    * Rename setup-prereqs.bash -> setup-prereqs.sh; make a few other changes
    
    * Automatically update npm to version 3
    
    * Split up apt-get installation of npm and mocha, since installing them together doesn't seem to be working
    
    * Add Ubuntu 16.04 setup script
    
    * More on Ubuntu 16.04 script
    
    * Add Node v4.4 to Travis
    
    * Make script/ubuntu/16.04/setup-prereqs.sh executable

--- test/mocha-test.js
+++ test/mocha-test.js
@@ -1,4 +1,8 @@
 var assert = require('assert');
+var chai = require('chai');
+var expect = chai.expect;
+var Cow = require( "../src/cow.js" ).Cow;
+
 describe('Array', function() {
 	  describe('#indexOf()', function () {
 		      it('should return -1 when the value is not present', function () {
@@ -6,4 +10,25 @@ describe('Array', function() {
 				          assert.equal(-1, [1,2,3].indexOf(0));
 					      });
 		        });
+});
+
+describe("Cow", function() {
+  describe("constructor", function() {
+    it("should have a default name", function() {
+      var cow = new Cow();
+      expect(cow.name).to.equal("Anon cow");
+    });
+
+    it("should set cow's name if provided", function() {
+      var cow = new Cow("Kate");
+      expect(cow.name).to.equal("Kate");
+    });
+  });
+
+  describe("#greets", function() {
+    it("should greet passed target", function() {
+      var greetings = (new Cow("Kate")).greets("Baby");
+      expect(greetings).to.equal("Kate greets Baby");
+    });
+  });
 });
